e460ddf957d29b4a64826da9867221979f51ac22
astevens37-gtri <andrew.stevens@gtri.gatech.edu>
2019-12-16 14:17:02 -0500

    Uploaded first set of files

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 14:13:35 2019
+
+@author: astevens37-gtri
+"""
+
+
+def test():
+    print('testing, 1, 2, 3')
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 14:10:15 2019
+
+@author: astevens37-gtri
+"""
+
+
+from setuptools import setup
+
+setup(name='PySpin Examples',
+      version='0.1',
+      description='Examples from FLIR\'s Spinnaker SDK',
+      url='https://github.com/astevens37/pyspinex',
+      author='Andy Stevens',
+      author_email='andrew.stevens@gtri.gatech.edu',
+      license='MIT',
+      packages=['pysinex'],
+      zip_safe=False)
